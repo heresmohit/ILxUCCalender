41f8f5164ba1869294338a900f4daf40bc78845c
Mohit Gumber <89568699+heresmohit@users.noreply.github.com>
2025-11-12 00:49:39 +0530

    Add SSL context to URL request in fetchdata.py

--- scripts/fetchdata.py
+++ scripts/fetchdata.py
@@ -1,10 +1,10 @@
 
-import ssl, urllib.request, json
-#ctx = ssl.create_default_context(cafile=certifi.where())
+import ssl,certifi, urllib.request, json
+ctx = ssl.create_default_context(cafile=certifi.where())
 
 url = "https://underline.center/latest.json"
 url2 = "https://underline.center/c/calendar/5.json"
-response = urllib.request.urlopen(url2)
+response = urllib.request.urlopen(url2,context = ctx)
 data = json.loads(response.read().decode("utf-8"))
 with open("dump.json", "w") as f:
    json.dump(data, f, indent=4)

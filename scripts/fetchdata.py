2665cfa2ca0fb4df25af27fafe9853c53559dea7
Mohit Gumber <89568699+heresmohit@users.noreply.github.com>
2025-11-12 00:47:57 +0530

    Simplify URL fetching by removing SSL context
    Removed SSL context creation for URL fetching.

--- scripts/fetchdata.py
+++ scripts/fetchdata.py
@@ -1,10 +1,10 @@
 
-import ssl, certifi, urllib.request, json
-ctx = ssl.create_default_context(cafile=certifi.where())
+import ssl, urllib.request, json
+#ctx = ssl.create_default_context(cafile=certifi.where())
 
 url = "https://underline.center/latest.json"
 url2 = "https://underline.center/c/calendar/5.json"
-response = urllib.request.urlopen(url2, context=ctx)
+response = urllib.request.urlopen(url2)
 data = json.loads(response.read().decode("utf-8"))
 with open("dump.json", "w") as f:
    json.dump(data, f, indent=4)
